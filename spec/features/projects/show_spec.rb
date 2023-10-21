b745e3f7770d84563387450dc270fd306bb54655
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-10-21 11:20:20 -0500

    US 3 complete

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -7,19 +7,32 @@ RSpec.describe "project show" do
     @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
     @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
 
-    @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
 
-    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
-    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
 
     visit "/projects/#{@news_chic.id}"
   end
 
-  describe "US 1- visit a projects show page" do
+  describe "US 1 - visit a projects show page" do
     it "I see the project's name, material and also the theme of the challenge this project belongs to" do
       expect(page).to have_content("Name: #{@news_chic.name}")
       expect(page).to have_content("Material: #{@news_chic.material}")
       expect(page).to have_content("Challenge Theme: #{@recycled_material_challenge.theme}")
+    end
+  end
+
+  describe "US 3 - visit a projects show page" do
+    it "I see a count of number of contestants on this project" do
+      save_and_open_page
+      expect(page).to have_content("Number of Contestants:")
     end
   end
 end
