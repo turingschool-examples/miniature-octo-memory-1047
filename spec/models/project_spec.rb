8b176527b1cc180eddcf07c48a9c241d51de013e
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-10-21 11:26:41 -0500

    refactor to model method

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,13 +2,32 @@ require "rails_helper"
 
 RSpec.describe Project, type: :model do
   describe "validations" do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :material}
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :material }
   end
 
   describe "relationships" do
-    it {should belong_to :challenge}
-    it {should have_many :contestant_projects}
-    it {should have_many(:contestants).through(:contestant_projects)}
+    it { should belong_to :challenge }
+    it { should have_many :contestant_projects }
+    it { should have_many(:contestants).through(:contestant_projects) }
+  end
+
+  describe "contestants_count" do
+    it "should tell me the number of contests on a project" do
+      @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+
+      @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+      @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+      @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+      ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @kentaro.id, project_id: @news_chic.id)
+
+      expect(@news_chic.contestants_count).to eq(3)
+    end
   end
 end
