2a4a59e67564f6753e39beba0c4dcea3672d88b8
Scott DeVoss <scottdevoss@yahoo.com>
2023-10-22 12:38:55 -0600

    added last test for us 2. Curious how I can make this test more robust

--- spec/features/contestants/index_spec.rb
+++ spec/features/contestants/index_spec.rb
@@ -17,6 +17,13 @@ RSpec.describe "Contestants Index Page", type: :feature do
         @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
         @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
         @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+        ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+        ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+        ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+        ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+        ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+        ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
       end
 
       #User Story 2
@@ -30,8 +37,13 @@ RSpec.describe "Contestants Index Page", type: :feature do
         # save_and_open_page
       end
 
-      xit "Under each contestants name I see a list of projects (names) that they've been on" do
+      it "Under each contestants name I see a list of projects (names) that they've been on" do
+        visit "/contestants"
 
+        expect(page).to have_content("News Chic")
+        expect(page).to have_content("Upholstery Tuxedo")
+        expect(page).to have_content("Boardfit")
+        expect(page).to_not have_content("Litfit")
       end
     end
   end
