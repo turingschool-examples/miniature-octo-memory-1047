a97198c0bfc572f254f6f3dd028fc7edd662a8ff
Eric Belongea <eric.belongea@gmail.com>
2023-10-23 11:26:57 -0600

    Sets up relationships

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -1,7 +1,7 @@
 class ContestantsController < ApplicationController
   def index
     @contestants = Contestant.all
-    @contestants = Contestant.projects
-    require 'pry'; binding.pry
+    # @contestants = Contestant.projects
+    # require 'pry'; binding.pry
   end
 end
--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -1,6 +1,7 @@
 class Contestant < ApplicationRecord
   validates_presence_of :name, :age, :hometown, :years_of_experience
-
+  has_many :contestant_projects
+  has_many :projects, through: :contestant_projects
   def self.projects
     require 'pry'; binding.pry
   end
