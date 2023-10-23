fd0e7c10d184cbde7a75b62fbc364d033152322f
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-23 06:59:17 -0600

    Add contestants count to projects index page and tests

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -10,13 +10,27 @@ RSpec.describe "projects show page", type: :feature do
     
     @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
     @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")    
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    @jay.projects << @lit_fit 
+    @jay.projects << @news_chic
+    @erin.projects << @news_chic
+    @erin.projects << @upholstery_tux
   end
   
   it "displays the project name and material and its challenge theme" do
     visit "/projects/#{@news_chic.id}"
 
     expect(page).to have_content(@news_chic.name)
-    expect(page).to have_content(@news_chic.material)
-    expect(page).to have_content(@recycled_material_challenge.theme)
+    expect(page).to have_content("Material: #{@news_chic.material}")
+    expect(page).to have_content("Challenge Theme: #{@recycled_material_challenge.theme}")
+  end
+
+  it 'displays a count of contestants on the project' do
+    visit "/projects/#{@news_chic.id}"
+save_and_open_page
+    expect(page).to have_content("Number of Contestants: 2")
   end
 end
