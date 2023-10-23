260853c9078e9ddf1236348c612697402acde42d
Eric Belongea <eric.belongea@gmail.com>
2023-10-23 11:49:31 -0600

    Adds testing for user story two

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -1,7 +1,5 @@
 class ContestantsController < ApplicationController
   def index
     @contestants = Contestant.all
-    # @contestants = Contestant.projects
-    # require 'pry'; binding.pry
   end
 end
--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,7 +2,9 @@ class Contestant < ApplicationRecord
   validates_presence_of :name, :age, :hometown, :years_of_experience
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
-  def self.projects
+  
+  def contestant_and_projects
+    
     require 'pry'; binding.pry
   end
 end
