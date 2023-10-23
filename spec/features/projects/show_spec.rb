c68f38c28a77a19e4e5c5f1caa4170e7f57169fe
Sam Tran <samuelttran@gmail.com>
2023-10-22 19:02:48 -0600

    Write initial test to have a form with id field

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -56,4 +56,25 @@ RSpec.describe 'Projects show', type: :feature do
     end
   end
 
+  describe 'When a user visits a project show page, they can add a contestant to a project through a form' do
+    before(:each) do
+      @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      @furniture_challenge = Challenge.create(theme: "Apartment Furnishings", project_budget: 1000)
+  
+      @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+  
+      @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+  
+      ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+      ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    end
+
+    it 'They see a form to add a contestant to this project, by existing contestant id' do
+      visit "projects/#{@upholstery_tux.id}"
+      expect(find("form")).to have_content("Contestant id")
+    end
+  end
 end
