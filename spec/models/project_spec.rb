b65688c31f19a324ffdac031cc83d4ad89a2de41
Kevin Zolman <kevin.zolman@gmail.com>
2023-10-23 06:45:43 -0600

    Write tests for project model #contestant_count

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -11,4 +11,30 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  before(:each) do
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    @boardfit = @recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes")
+    
+    @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
+    @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")    
+
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+
+    @jay.projects << @boardfit
+    @jay.projects << @upholstery_tux
+    @erin.projects << @news_chic
+    @erin.projects << @upholstery_tux
+  end
+
+  describe "instance methods" do
+    describe "#contestant_count" do
+      it "returns a count of contestants on a given project" do
+        expect(@upholstery_tux.contestant_count).to eq(2)
+        expect(@boardfit.contestant_count).to eq(1)
+        expect(@lit_fit.contestant_count).to eq(0)
+      end
+    end
+  end
 end
