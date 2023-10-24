47f30b3e8d61aa59e55047c5ab1d1d5f53faab6e
Cory Powell <coryrpowell@gmail.com>
2023-10-23 21:24:51 -0400

    added model testing for the con_count method in the projects model

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -21,12 +21,32 @@ RSpec.describe Project, type: :model do
     
     @upholstery_tux = @furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch")
     @lit_fit = @furniture_challenge.projects.create(name: "Litfit", material: "Lamp")
+  
+  
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+    @kentaro = Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8)
+    @erin = Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15)
+  
+  
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @upholstery_tux.id)
+    ContestantProject.create(contestant_id: @kentaro.id, project_id: @boardfit.id)
+    ContestantProject.create(contestant_id: @erin.id, project_id: @boardfit.id)
   end
 
   describe "#instance methods" do
     describe "challenge_theme" do 
       it "shows the theme from the challenge that this project belongs_to" do
         expect(@news_chic.challenge_theme).to eq(@recycled_material_challenge.theme)
+      end
+    end
+
+    describe "count" do
+      it "shows the count of the number of contestants on this project" do
+        expect(@news_chic.con_count).to eq(2)
       end
     end
   end
