cc44e1348b3045187a496f4c35429e2ba18232f3
Kam Kennedy <kameronk013@gmail.com>
2023-10-24 15:35:27 -0600

    complete extension 1

--- spec/features/project/show_spec.rb
+++ spec/features/project/show_spec.rb
@@ -2,22 +2,21 @@ require "rails_helper"
 
 RSpec.describe "the project show page" do
   before(:each) do
+    @recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
 
+    @news_chic = @recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+    
+    @jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+    @gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
+
+    ContestantProject.create(contestant_id: @jay.id, project_id: @news_chic.id)
+    ContestantProject.create(contestant_id: @gretchen.id, project_id: @news_chic.id)
   end
 
   it "shows details about the project and the theme of associated challenge" do
-    recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
-
-    news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
-
-    jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
-    gretchen = Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12)
-
-    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
-    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
   # As a visitor,
 
-    visit("/projects/#{news_chic.id}")
+    visit("/projects/#{@news_chic.id}")
   # When I visit a project's show page ("/projects/:id"),
   # I see that project's name and material
     expect(page).to have_content("News Chic")
@@ -30,14 +29,29 @@ RSpec.describe "the project show page" do
   end
 
   it "shows how many contestants on each project" do
+
     #     As a visitor,
     # When I visit a project's show page
-    visit("/projects/#{news_chic.id}")
+    visit("/projects/#{@news_chic.id}")
     # I see a count of the number of contestants on this project
     expect(page).to have_content("2")
     # (e.g.    Litfit
     #     Material: Lamp Shade
     #   Challenge Theme: Apartment Furnishings
     #   Number of Contestants: 3 )
+  end
+
+  it "shows average experience of contestants on each project" do
+   
+    # As a visitor,
+    # When I visit a project's show page
+    visit("/projects/#{@news_chic.id}")
+    # I see the average years of experience for the contestants that worked on that project
+    expect(page).to have_content("12.5")
+    # (e.g.    Litfit
+    #     Material: Lamp Shade
+    #   Challenge Theme: Apartment Furnishings
+    #   Number of Contestants: 3
+    #   Average Contestant Experience: 10.25 years)
   end
 end
