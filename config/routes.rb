039bb7f1f86ea99bb7cbdd73eb6fab75cb8edec8
Justin Lee <140270202+JustinSteel@users.noreply.github.com>
2023-10-24 14:28:26 -0600

    complete user story 2 all tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/projects/:id", to: "projects#show"
+  get "/contestants", to: "contestants#index"
 end
