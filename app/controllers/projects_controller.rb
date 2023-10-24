728e06be80f3940e5249b45a8f8223e8291b9de5
Nathan Trautenberg <Natester123@mac.com>
2023-10-24 17:01:20 -0600

    add number of contestants to project show page, update controller to make it function, and passed test.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,8 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @challenge = @project.challenge
+    # @contestants_count = ContestantProject.where(project: @project).count
+    @contestants_count = ContestantProject.all.where("project_id = #{@project.id}").count
   end
 
 end
