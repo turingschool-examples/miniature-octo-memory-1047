d58d85437d1afebd786bb346565b0d56fcf879b5
Xander Hendry <vhendry.coding@gmail.com>
2023-10-25 12:23:27 -0400

    add test to models/projects to tests proper functionality of #project_challeng_theme, and add create features/contestants/index_spec testing that contestant index page is properly displaying contents.

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe Project, type: :model do
     it {should have_many :contestant_projects}
     it {should have_many(:contestants).through(:contestant_projects)}
   end
+
+  describe "#project_challenge_theme" do 
+    it "lists the theme of the challenge a project belongs too" do 
+      recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+
+      expect(news_chic.project_challenge_theme).to eq("Recycled Material")
+    end
+  end
 end
