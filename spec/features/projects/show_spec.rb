d62c0576d29bdf7d13e622b7c92948c18c0f9649
Xander Hendry <vhendry.coding@gmail.com>
2023-10-25 12:38:26 -0400

    add test showing that the project index page will show number of contestants on that project, and add a test to project_spec that tests project model method #count_contestants works as intended

--- spec/features/projects/show_spec.rb
+++ spec/features/projects/show_spec.rb
@@ -7,6 +7,20 @@ RSpec.describe "Projects show page (/projects/:id)" do
   let(:boardfit) { recycled_material_challenge.projects.create(name: "Boardfit", material: "Cardboard Boxes") } 
   let(:upholstery_tux) { furniture_challenge.projects.create(name: "Upholstery Tuxedo", material: "Couch") } 
   let(:lit_fit) { furniture_challenge.projects.create(name: "Litfit", material: "Lamp Shade") } 
+  let(:jay) { Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13) } 
+  let(:gretchen) { Contestant.create(name: "Gretchen Jones", age: 36, hometown: "NYC", years_of_experience: 12) } 
+  let(:kentaro) { Contestant.create(name: "Kentaro Kameyama", age: 30, hometown: "Boston", years_of_experience: 8) } 
+  let(:erin) { Contestant.create(name: "Erin Robertson", age: 44, hometown: "Denver", years_of_experience: 15) } 
+  
+  before(:each) do 
+    ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: news_chic.id)
+    ContestantProject.create(contestant_id: gretchen.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: upholstery_tux.id)
+    ContestantProject.create(contestant_id: kentaro.id, project_id: boardfit.id)
+    ContestantProject.create(contestant_id: erin.id, project_id: boardfit.id)
+    ContestantProject.create(contestant_id: jay.id, project_id: boardfit.id)
+  end
   
   describe " When I visit a projects show page (/projects/:id)" do 
     it "shows that projects name and material, as well as the theme of the challenge the project belongs to" do 
@@ -15,6 +29,14 @@ RSpec.describe "Projects show page (/projects/:id)" do
       expect(page).to have_content("Litfit")
       expect(page).to have_content("Material: Lamp Shade")
       expect(page).to have_content("Challenge Theme: Apartment Furnishings")
+    end
+
+    it "shows a count of the number of contestants on this project" do 
+      visit "/projects/#{boardfit.id}"
+      expect(page).to have_content("Boardfit")
+      expect(page).to have_content("Material: Cardboard Boxes")
+      expect(page).to have_content("Challenge Theme: Recycled Material")
+      expect(page).to have_content("Number of Contestants: 3")
     end
   end
   
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -20,4 +20,15 @@ RSpec.describe Project, type: :model do
       expect(news_chic.project_challenge_theme).to eq("Recycled Material")
     end
   end
+
+  describe "#count_contestants" do 
+    it "will count all contestants assigned to a project" do 
+       recycled_material_challenge = Challenge.create(theme: "Recycled Material", project_budget: 1000)
+      news_chic = recycled_material_challenge.projects.create(name: "News Chic", material: "Newspaper")
+      jay = Contestant.create(name: "Jay McCarroll", age: 40, hometown: "LA", years_of_experience: 13)
+      ContestantProject.create(contestant_id: jay.id, project_id: news_chic.id)
+
+      expect(news_chic.count_contestants).to eq(1)
+    end
+  end
 end
